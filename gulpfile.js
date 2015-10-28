42fe1eae0ea1752f39e9f4c964d71073dfcf06bd
gasolin <gasolin@gmail.com>
2015-10-28 11:30:16 +0800

    marketplace ready

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('default', function() {
-  return gulp.src(['main.js', 'manifest.webapp', 'LICENSE', '**/icons/*'])
-    .pipe(zip('application.zip'))
+  return gulp.src(['main.js', 'manifest.json', 'update.webapp', 'LICENSE', '**/icons/*'])
+    .pipe(zip('package.zip'))
     .pipe(gulp.dest('./'));
 });
