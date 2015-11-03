a20622ef751a99e7d02e0416e675031ff29184b7
gasolin <gasolin@gmail.com>
2015-11-03 23:11:48 +0800

    remove update.webapp and update gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('default', function() {
-  return gulp.src(['main.js', 'manifest.json', 'update.webapp', 'LICENSE', '**/icons/*'])
+  return gulp.src(['main.js', 'manifest.json', 'LICENSE', '**/icons/*'])
     .pipe(zip('package.zip'))
     .pipe(gulp.dest('./'));
 });
